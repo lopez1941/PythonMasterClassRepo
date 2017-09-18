30f4bdc7554adcef4389fc6539a218deeeb03146
lopez1941 <guitarlopez33@gmail.com>
2017-09-18 19:44:58 -0400

    a little late night work

--- DatesAndDateTime/dateandtime.py
+++ DatesAndDateTime/dateandtime.py
@@ -7,3 +7,6 @@ print("the current time zone is {0} with an offset of {1}".format(time.tzname[0]
 if time.daylight != 0:
     print("\tDaylight savings time is in effect for this location.")
     print("\tThe DST timezone is " + time.tzname[1])
+
+print("Local time is " + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+print("UTC time is " + time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime()))
