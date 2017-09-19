72c074bc598dc01db87f1a8e9af2a2fde4be1448
lopez1941 <guitarlopez33@gmail.com>
2017-09-19 09:29:07 -0400

    updating dateAndTime project

--- DatesAndDateTime/dateandtime.py
+++ DatesAndDateTime/dateandtime.py
@@ -1,12 +1,18 @@
-import time
+# import time
+#
+# print(" the epoch starts at " + time.strftime('%c', time.gmtime(0)))
+#
+# print("the current time zone is {0} with an offset of {1}".format(time.tzname[0], time.timezone))
+#
+# if time.daylight != 0:
+#     print("\tDaylight savings time is in effect for this location.")
+#     print("\tThe DST timezone is " + time.tzname[1])
+#
+# print("Local time is " + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+# print("UTC time is " + time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime()))
 
-print(" the epoch starts at " + time.strftime('%c', time.gmtime(0)))
+import datetime
 
-print("the current time zone is {0} with an offset of {1}".format(time.tzname[0], time.timezone))
-
-if time.daylight != 0:
-    print("\tDaylight savings time is in effect for this location.")
-    print("\tThe DST timezone is " + time.tzname[1])
-
-print("Local time is " + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
-print("UTC time is " + time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime()))
+print(datetime.datetime.today())
+print(datetime.datetime.now())
+print(datetime.datetime.utcnow())
