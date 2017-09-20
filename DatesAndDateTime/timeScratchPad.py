19d9ee8ba0ed76482427f13888e2bd8db8f15d44
lopez1941 <guitarlopez33@gmail.com>
2017-09-19 21:39:20 -0400

    moved code to timechallenge.py

--- DatesAndDateTime/timeScratchPad.py
+++ DatesAndDateTime/timeScratchPad.py
@@ -14,10 +14,6 @@ us_zone_dict = {'1': ("US/Eastern", pytz.timezone('US/Eastern')),
                 '8': ("Europe/London", pytz.timezone('Europe/London')),
                 '9': ("Australia/Darwin", pytz.timezone('Australia/Darwin'))}
 
-
-# choice = 1
-# if choice in us_zone_dict.keys():
-#     print(choice, us_zone_dict[choice][0])
 user_choice = '1'
 print("Enter 0 to quit!")
 while user_choice != '0':
@@ -30,6 +26,7 @@ while user_choice != '0':
               .format(chosen_zone, utc_time_now.astimezone(py_zone_entry)))
         print("UTC time is {0}".format(utc_time_now))
         print("Local time is {0}".format(local_time_aware))
+        print("=" * 50)
 
     elif user_choice == '0':
         print("Sorry you want to quit!")
