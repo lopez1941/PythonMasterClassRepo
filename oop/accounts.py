3a256287244337c9b0eadd4e1e7fd3ee14db4517
lopez1941 <guitarlopez33@gmail.com>
2017-09-29 15:56:40 -0400

    "commit"

--- oop/accounts.py
+++ oop/accounts.py
@@ -8,11 +8,14 @@ class Account:
     def __init__(self, name, balance):
         self.name = name
         self.balance = balance
+        self.starting_balance = balance
         print("Account created for " + self.name)
+        print("Balance is {}".format(self.balance))
         self.transaction_list = []
 
     def deposit(self, amount):
         if amount > 0:
+            print("Depositing {}".format(amount))
             self.balance += amount
             self.show_balance()
             # appending date/time to transaction_list. localizing utc time using pytz
@@ -20,7 +23,9 @@ class Account:
 
     def withdrawal(self, amount):
         if 0 < amount <= self.balance:
+            print("Withdrawing {}".format(amount))
             self.balance -= amount
+            self.transaction_list.append((pytz.utc.localize(datetime.datetime.utcnow()), -amount))
         else:
             print("The amount must be greater than 0 and no greater than account balance.")
         self.show_balance()
@@ -29,6 +34,7 @@ class Account:
         print("Balance is {0}".format(self.balance))
 
     def show_transactions(self):
+        print("Starting balance was {}:".format(self.starting_balance))
         for date, amount in self.transaction_list:
             if amount > 0:
                 tran_type = "deposited"
@@ -38,8 +44,13 @@ class Account:
             print("{:6} {} on {} (local time was {})".format(amount, tran_type, date, date.astimezone()))
 
 if __name__ == '__main__':
-    donnie = Account("Donnie", 0)
-    donnie.deposit(1000)
-    donnie.withdrawal(5000)
-    donnie.withdrawal(50)
-    donnie.show_transactions()
+    # donnie = Account("Donnie", 0)
+    # donnie.deposit(1000)
+    # donnie.withdrawal(5000)
+    # donnie.withdrawal(50)
+    # donnie.show_transactions()
+
+    steph = Account("Steph", 800)
+    steph.deposit(100)
+    steph.withdrawal(200)
+    steph.show_transactions()
