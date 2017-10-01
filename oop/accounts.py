c4afe71df77cfd66cfc8ac3be0bb4fb1e08f79f0
lopez1941 <guitarlopez33@gmail.com>
2017-09-30 21:55:43 -0400

    "commit"

--- oop/accounts.py
+++ oop/accounts.py
@@ -4,38 +4,44 @@ import pytz
 
 class Account:
     """ Simple account class with balance """
+
+    @staticmethod
+    def _current_time():
+        utc_time = datetime.datetime.utcnow()
+        return pytz.utc.localize(utc_time)
+
+
     # first method called is 'new'. init method then updates
     def __init__(self, name, balance):
-        self.name = name
-        self.balance = balance
-        self.starting_balance = balance
-        print("Account created for " + self.name)
-        print("Balance is {}".format(self.balance))
-        self.transaction_list = []
+        self._name = name
+        self.__balance = balance
+        print("Account created for " + self._name)
+        print("Balance is {}".format(self.__balance))
+        self._transaction_list = [(Account._current_time(), balance)]
+        self.show_balance()
 
     def deposit(self, amount):
         if amount > 0:
             print("Depositing {}".format(amount))
-            self.balance += amount
+            self.__balance += amount
             self.show_balance()
             # appending date/time to transaction_list. localizing utc time using pytz
-            self.transaction_list.append((pytz.utc.localize(datetime.datetime.utcnow()), amount))
+            self._transaction_list.append((pytz.utc.localize(datetime.datetime.utcnow()), amount))
 
     def withdrawal(self, amount):
-        if 0 < amount <= self.balance:
+        if 0 < amount <= self.__balance:
             print("Withdrawing {}".format(amount))
-            self.balance -= amount
-            self.transaction_list.append((pytz.utc.localize(datetime.datetime.utcnow()), -amount))
+            self.__balance -= amount
+            self._transaction_list.append((pytz.utc.localize(datetime.datetime.utcnow()), -amount))
         else:
             print("The amount must be greater than 0 and no greater than account balance.")
         self.show_balance()
 
     def show_balance(self):
-        print("Balance is {0}".format(self.balance))
+        print("Balance is {0}".format(self.__balance))
 
     def show_transactions(self):
-        print("Starting balance was {}:".format(self.starting_balance))
-        for date, amount in self.transaction_list:
+        for date, amount in self._transaction_list:
             if amount > 0:
                 tran_type = "deposited"
             else:
@@ -44,13 +50,15 @@ class Account:
             print("{:6} {} on {} (local time was {})".format(amount, tran_type, date, date.astimezone()))
 
 if __name__ == '__main__':
-    # donnie = Account("Donnie", 0)
-    # donnie.deposit(1000)
-    # donnie.withdrawal(5000)
-    # donnie.withdrawal(50)
-    # donnie.show_transactions()
+    donnie = Account("Donnie", 0)
+    donnie.deposit(1000)
+    donnie.withdrawal(5000)
+    donnie.withdrawal(50)
+    donnie.show_transactions()
 
     steph = Account("Steph", 800)
+    steph.__balance = 200
     steph.deposit(100)
     steph.withdrawal(200)
     steph.show_transactions()
+    print(steph.__dict__)
