90854215da0a97a8177782d87222eec7fefa497e
lopez1941 <guitarlopez33@gmail.com>
2017-10-02 16:49:55 -0400

    commit

--- game/player.py
+++ game/player.py
@@ -4,7 +4,7 @@ class Player(object):
         self.name = name
         self._lives = 3
         self._level = 1
-        self.score = 0
+        self._score = 0
 
     def _get_lives(self):
         return self._lives
@@ -22,7 +22,7 @@ class Player(object):
     def _set_level(self, level):
         if level >= 1:
             difference = level - self._level
-            self.score += difference * 1000
+            self._score += difference * 1000
             self._level = level
         else:
             print("Level can't be less than one!")
@@ -32,6 +32,13 @@ class Player(object):
 
     level = property(_get_level, _set_level)
 
+    @property
+    def score(self):
+        return self._score
+
+    @score.setter
+    def score(self, score):
+        self._score = score
 
     def __str__(self):
         return "Name: {0.name}, Lives: {0.lives}, Level: {0.level}, Score: {0.score}".format(self)
---
+++ oop/song.py
@@ -0,0 +1,9 @@
+class Song:
+    """Not messing with the doc string stuff"""
+
+    def __init__(self, title, artist, duration=0):
+
+        self.title = title
+        self.artist = artist
+        self.duration = duration
+        
