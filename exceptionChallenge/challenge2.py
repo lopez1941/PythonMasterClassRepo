f379579d36bfcfd47b82b8b9e6f2b25709de34bf
lopez1941 <guitarlopez33@gmail.com>
2017-10-13 17:09:45 -0400

    commit

--- exceptionChallenge/challenge2.py
+++ exceptionChallenge/challenge2.py
@@ -1,3 +1,5 @@
+import sys
+
 def get_user_input():
     while True:
         try:
@@ -5,6 +7,10 @@ def get_user_input():
             return input_received
         except ValueError:
             print("Oops, try again.  That wasn't a valid integer!")
+        except EOFError:
+            sys.exit(1)
+        finally:
+            print("The finally clause always executes")
 
 
 number1 = get_user_input()
@@ -14,4 +20,7 @@ try:
     print("The first number divided by the second number is {}".format(number1 / number2))
 except ZeroDivisionError:
     print("Whoa, there.  You can't divide by zero, dude!")
+    sys.exit(2)
+else:  # else is only executed if the try completes, finally will execute regardless
+    print("Division performed successfully!")
 
