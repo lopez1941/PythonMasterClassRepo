9822962dca560467db8eda662814b46a85ec793f
lopez1941 <guitarlopez33@gmail.com>
2017-10-18 12:06:50 -0400

    commit

--- game/ducks.py
+++ game/ducks.py
@@ -29,6 +29,8 @@ class Duck(object):
         self._wing.fly()
 
 class Penguin(object):
+    def __init__(self):
+        self.fly = self.aviate
     def walk(self):
         print("Waddle, waddle, I waddle too")
 
@@ -38,16 +40,36 @@ class Penguin(object):
     def quack(self):
         print("Are you havin' a laugh? I'm a penguin!")
 
+    def aviate(self):
+        print("I won the lottery and won a jet!")
+
+
+class Mallard(Duck):
+    pass
+
 class Flock(object):
     def __init__(self):
         self.flock = []
 
-    def add_duck(self, duck):
-        self.flock.append(duck)
+    def add_duck(self, duck: Duck) -> None:
+        fly_method = getattr(duck, 'fly', None)
+        if callable(fly_method):
+            self.flock.append(duck)
+        else:
+            raise TypeError("Cannot add duck, are you sure it's not a " + str(type(duck).__name__))
 
     def migrate(self):
+        problem = None
         for duck in self.flock:
-            duck.fly()
+            try:
+                duck.fly()
+                raise AttributeError("Testing exception handler in migrate")
+            except AttributeError as e:
+                print("One duck down")
+                #raise
+                problem = e
+            if problem:
+                raise problem
 
 # def test_duck(duck):
 #     duck.walk()
