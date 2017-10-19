3b32e63cb7605be629ebc6878554399daa6f7194
lopez1941 <guitarlopez33@gmail.com>
2017-10-19 16:24:31 -0400

    commit

--- RollingBack/checkdb.py
+++ RollingBack/checkdb.py
@@ -1,12 +1,12 @@
 import sqlite3
 
+
 db = sqlite3.connect("accounts.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)
 
 
-for row in db.execute("SELECT * FROM history"):
-    #print(row)
-    local_time = row[0]
-    print("{}\t{}".format(local_time, type(local_time)))
-
+# for row in db.execute("SELECT strftime('%Y-%m-%d %H:%M:%f', history.time, 'localtime') AS localtime,"
+#                       "history.account, history.amount FROM history ORDER BY history.time"):
+for row in db.execute("select * from localhistory"):
+    print(row)
 
 db.close()
--- RollingBack/rollback.py
+++ RollingBack/rollback.py
@@ -3,18 +3,19 @@ import datetime
 import pytz
 
 
-db = sqlite3.connect("accounts.sqlite")
+db = sqlite3.connect("accounts.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)
 db.execute("CREATE TABLE IF NOT EXISTS accounts (name TEXT PRIMARY KEY NOT NULL, balance INTEGER NOT NULL)")
 db.execute("CREATE TABLE IF NOT EXISTS history (time TIMESTAMP NOT NULL, account TEXT NOT NULL, "
            "amount INTEGER NOT NULL, PRIMARY KEY (time, account))")  # composite primary key combo of time and account
+db.execute("CREATE VIEW IF NOT EXISTS localhistory "
+           "as SELECT strftime('%Y-%m-%d %H:%M:%f', history.time, 'localtime') AS localtime,"
+           "history.account, history.amount FROM history ORDER BY history.time")
 
 
 class Account(object):
     @staticmethod
     def _current_time():
-        # return pytz.utc.localize(datetime.datetime.utcnow())
-        local_time = pytz.utc.localize(datetime.datetime.utcnow())
-        return local_time.astimezone()
+        return pytz.utc.localize(datetime.datetime.utcnow())
 
     def __init__(self, name: str, opening_balance: int = 0):
         cursor = db.execute("SELECT name, balance FROM accounts WHERE (name = ?)", (name,))
@@ -35,10 +36,14 @@ class Account(object):
     def _save_update(self, amount):
         new_balance = self._balance + amount
         deposit_time = Account._current_time()
-        db.execute("UPDATE accounts SET balance = ? WHERE (name = ?)", (new_balance, self.name))
-        db.execute("INSERT INTO history VALUES(?, ?, ?)", (deposit_time, self.name, amount))
-        db.commit()
-        self._balance = new_balance
+        try:
+            db.execute("UPDATE accounts SET balance = ? WHERE (name = ?)", (new_balance, self.name))
+            db.execute("INSERT INTO history VALUES(?, ?, ?)", (deposit_time, self.name, amount))
+        except sqlite3.Error:
+            db.rollback()
+        else:
+            db.commit()
+            self._balance = new_balance
 
     def deposit(self, amount: int) -> float:
         if amount > 0.0:
